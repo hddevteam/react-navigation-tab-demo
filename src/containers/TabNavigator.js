004ff522c51b8cdb092200a5e5ad74bea9540cbb
hddevteam <mengmengxiao@msn.com>
2019-12-05 22:30:34 +0800

    Now the tab icons can be displayed. TODO: display tint color when focused.

--- src/containers/TabNavigator.js
+++ src/containers/TabNavigator.js
@@ -1,10 +1,12 @@
-import {StyleSheet, Dimensions, Platform, NativeModules} from 'react-native'
+import React from 'react';
+import { StyleSheet, Image } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import HomePicturePage from '../pages/HomePicturePage';
 import DownloadPage from '../pages/DownloadPage';
 import RemoteControlPage from '../pages/RemoteControlPage';
 import DevicesPage from '../pages/DevicesPage';
 import SmartScale from '../entity/SmartScale';
+import homeIcon from '../resources/labelIcon/photo.png'
 
 
 export const TabNavigator = createBottomTabNavigator({
@@ -14,6 +16,36 @@ export const TabNavigator = createBottomTabNavigator({
     Devices: DevicesPage
 },
     {
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+                const { routeName } = navigation.state;
+                if (routeName === 'Home') {
+                    return (
+                        <Image
+                            source={require('../resources/labelIcon/photo.png')
+                            }
+                            style={{
+                                width: 20,
+                                height: 20,
+                                borderRadius: 40 / 2,
+                            }}
+                        />
+                    );
+                } else if (routeName === 'Devices') {
+                    return (
+                        <Image
+                            source={require('../resources/labelIcon/photo.png')
+                            }
+                            style={{
+                                width: 20,
+                                height: 20,
+                                borderRadius: 40 / 2,
+                            }}
+                        />
+                    );
+                }
+            },
+        }),
         tabBarOptions: {
             animationEnabled: true,
             upperCaseLabel: false,
--- src/pages/HomePicturePage.js
+++ src/pages/HomePicturePage.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, Image, View } from 'react-native';
+import homeIcon from '../resources/labelIcon/photo.png'
 
 class HomePicturePage extends React.Component {
+  // //Tab导航的样式
+  // static navigationOptions = ({ navigation, screenProps }) => {
+  //   return {
+  //     tabBarLabel: strings.HomePictureTabName,
+  //     tabBarIcon: ({ tintColor }) => (
+  //       <View style={styles.contentStyle}>
+  //         <Image
+  //           source={require('../resources/labelIcon/photo.png')}
+  //           style={[labelStyles.icon, { tintColor: tintColor }]}
+  //         />
+  //       </View>)
+  //   }
+  // }
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Home!</Text>
+        <Image
+          source={homeIcon}
+        />
       </View>
     );
   }
